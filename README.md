b9e77330175cff76ce6f5bd5faa089d7e95a5098
robbyikusuma88@gmail.com <robbyikusuma88@gmail.com>
2022-06-19 01:36:34 +0700

    Latihan JS 1

---
+++ README.md
@@ -0,0 +1,3 @@
+#Progate-Belajar Java Script
+
+<h2>Latihan Javascript ke-1</h2>
---
+++ page1/script.js
@@ -0,0 +1,3 @@
+console.log("Selamat datang di ES6 Study I!");
+console.log("Ayo belajar bersama Ninja Ken!");
+console.log("Hello World")
